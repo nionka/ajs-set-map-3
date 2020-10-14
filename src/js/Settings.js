9e7ad84681e7fb0dfd242cf76a655879927f6189
Olga Runova <ockaaa@gmail.com>
2020-10-14 14:58:07 +0300

    fix

--- src/js/Settings.js
+++ src/js/Settings.js
@@ -12,11 +12,18 @@ export default class Settings {
     ];
     this.usersSettings = new Map(this.defaultSettings);
 
+    // было так
+    // rest.forEach((elem) => {
+    //   for (const key in elem) {
+    //     if (Object.prototype.hasOwnProperty.call(elem, key)) {
+    //       this.usersSettings.set(key, elem[key]);
+    //     }
+    //   }
+    // });
+
     rest.forEach((elem) => {
-      for (const key in elem) {
-        if (Object.prototype.hasOwnProperty.call(elem, key)) {
-          this.usersSettings.set(key, elem[key]);
-        }
+      for (const [key, value] of Object.entries(elem)) {
+        this.usersSettings.set(key, value);
       }
     });
   }
